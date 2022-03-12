3acbc09ad12d68f8ddbad944af62fabf54500b0d
Furkan Keskinkilic <furkankeskinkilic97@gmail.com>
2022-03-13 00:05:43 +0300

    search form  added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -40,9 +42,37 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: "You must provide an address"
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if(error){
+            return res.send({ error:error })
+        }
+        forecast(latitude, longitude, ( error,  forecastData) => {
+            if(error){
+                return res.send({ error:error })
+
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+
+    })
+    // res.send({
+    //     forecast: 'It is snowing',
+    //     location: 'Philadelphia',
+    //     address: req.query.address
+    // })
+})
+app.get('/products', (req, res) => {
     res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
+        products: []
     })
 })
 app.get('/help/*', (req,res) =>{
